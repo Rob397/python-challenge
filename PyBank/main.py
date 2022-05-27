0b998e8549a2390be5481a7ea94fcbabe72b9257
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-05-27 21:02:00 +1000

    Update to homework
    Finished PyPoll, cleaned Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,28 +6,20 @@
 #The greatest increase in profits (date and amount) over the entire period
 #The greatest decrease in losses (date and amount) over the entire period
 
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-# First task is to read and open the csv file and skip that first line with the title
-
-   # checked for the csv file to work print(f"Header:{csv_header}")
-
-# total number of months: we just need to set up a count for the total number of 
-
-# cells/data points in the month coloumn which is the range function
-
-# Total profit/Loss is adding all the elements in the P&L column
-
+# Notes on Calculations
 # Average of the changes of P&L is two calculations  
 # (difference between each cell) = changes and sum(changes)/range(columns)
 
 # greatest increase/decreases are the maximum of the change in profit or loss and then find/show the relevant row of data
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
 
 # Modules
 from operator import index
 import os
 import csv
-#import numpy as np ---- Does not work!
+
 count_m = 0
 row_count = 0 # count the number of row for the for loop to get the number of month/data points
 
@@ -39,78 +31,38 @@ month = []
 pnl =[]
 change =[]
 count =0
-pnl_total =0
+
 list=[]
 count = 0
 total =0
+
+total_diff =[]
+
+
+# pnl1 = pnl[1]-pnl[0]
  #Open the CSV
  # define function above the with open??
 with open(csvpath) as csvfile:
-
+  
   csvreader = csv.reader(csvfile, delimiter=",")
   
   header =next(csvreader)
   for row in csvreader:
         
-        # count  = count + 1
-        # print(header)
+        
         # Add month
         month.append(row[0])
+        # Make month list
+        pnl.append(row[1])
+        # make pnl list
         count = count +1
-        # add  pnl
-        
-        # del(pnl[0])
-        # pnl.append(row[1])
+    
         total += int(row[1])    #int(row[1])   # total = total + int(row[1]) 
+    
 print("Financila Analysis")
 print("--------------------------------")
-print(f"Total Months:     {count}" )
-print(f"The total profit/loss:   {total}") 
-
-        # for val in range(0, len(pnl)):
-        #  total = total + pnl[val]
-        
-        
-
-
-
-
-
-
-
-
-        
-
-
-        # print("Financial Analysis ")
-        # print("-------------------")
-        # print (f"Total Months:{len(month)-1}") 
-        # print(f"The total profit/loss: {pnlcount}")
-    
-    
-
-
-
-
-
-
-
-
-
-
-    # for row in csvreader:
-    #   #add up the total number of columns after the first row.
-    #   count_m += int(row)    #int(row[1])   # total = total + int(row[1]) 
-    #   total = int(row[1])
-    #   totalpnl =(row[1])
-    # print("Financial Analysis ")
-    # print("-------------------")
-    # print (f"Total Months:{len(rows)-1}") 
-    # print(f"The total profit/loss: {totalpnl}")
-
-
-      
-
+print (f"Total Months:{len(month)}")
+print(f"The total profit/loss:   {total}")
 
 
 
@@ -124,24 +76,7 @@ print(f"The total profit/loss:   {total}")
 
 
     
-   
-    # for j in range(1,len(rows)):           # j from 1 to length-1
-    #  k = j - 1                             # set the new k in the first outer loop
-    #  row_list = []                         # reset row_list each time
-     
-    # for i in range (j, len(rows)):         # i from j to len-1
-    #        change = (rows[i] - rows[k])    # get difference between each cell
-    #        row_list.append(change)
-    # print(row_list)                        # print out the distances for each outer loop iteration
-   
   
-    # split the pnl and month then add up the index 1 (P&L column) values
-    
-    #for line in rows:
-      #   #Type = line.split(",")
-      #   x = Type[1]
-      #   y = Type[2]
-        # print(rows)
 
 
         
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,32 +1,75 @@
 # The dataset is composed of three columns: Voter ID, County, and Candidate. 
 # Your task is to create a Python script that analyses the votes and calculates each of the following:
 # 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
+# 2. A complete list of candidates who received votes ( = find all the uniuqe strings in a list)
+# 3. The percentage of votes each candidate won.      (= votes for candidate/total number if votes cast)
+# 4. The total number of votes each candidate won. 
 # 5. The winner of the election based on popular vote.
-
+from collections import OrderedDict
+import operator
 import os
 import csv
-
-
+from re import A
+list_ID = []
+list_country =[]
+list_Candidate =[]
+counter = 0
+ 
 # Set path for file
 csvpath = os.path.join("Resources", "election_data.csv")
+# Percentage of votes won
 
- #Open the CSV
- # define function above the with open??
+ 
 with open(csvpath) as csvfile:
      csvreader = csv.reader(csvfile, delimiter=",")
-      #csv_header = next(csvreader)   #an put print(csv_header) to check it worked
-       #print (rows[1])  # print the second row
-  
-     rows = list(csvreader)
+     csv_header = next(csvreader)   
+                                   #print (rows[1])  # print the second row
      
-     print("Election Results")
-     print("----------------------------------")
-     print (f"Total Number of Votes Cast:{len(rows)-1}")      
-     
+
+    #  rows = list(csvreader)
+     for row in csvreader:
+      list_ID.append(row[0])
+      list_country.append(row[1])
+      list_Candidate.append(row[2])
+      
+
+x = list_Candidate.count("Khan")
+y = list_Candidate.count("Correy")
+z= list_Candidate.count("Li")
+a= list_Candidate.count("O'Tooley")
+#percentages
+percent_x= 100*x/len(list_ID)
+percent_y= 100*y/len(list_ID)
+percent_z= 100*z/len(list_ID)
+percent_a= 100*a/len(list_ID)
+
+# Dictionary with values of results so winner can be automated
+Dict = {"Khan" : x, "O'Tooley": a, "Corry" : y, "Li" : z  }
+new_ma_val = max(Dict.items(), key=operator.itemgetter(1))[0]
+print(f"The winner is {new_ma_val}")     
+ 
+
+unique_list = list(dict.fromkeys(list_Candidate))
+
+#Simple version by checking the length and puttin in the Canidate results {unique_list[0]}:  {round(percent_x,3)}%     ({x}) manually for each candidate up to len(list_Candidate)
   
-    
+print("Election Results")
+print("----------------------------------")
+print (f"Total Number of Votes Cast:{len(list_ID)}")  
+print("----------------------------------")
+print(f"{unique_list[0]}:  {round(percent_x,3)}%     ({x})  \n{unique_list[1]}:  {round(percent_y,3)}%      ({y}) \n{unique_list[2]} :    {round(percent_z,3)}%     ({z})  \n{unique_list[3]}:   {round(percent_a,3)}%     ({a})")
+print("----------------------------------")
+print(f"The winner is {new_ma_val}") 
+print("----------------------------------")
+
+
+
+
+
+
+
+
+
+
 
 
