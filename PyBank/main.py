be8acef0b13f6d0e8049d396212f263d4e6ac366
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-05-28 15:17:44 +1000

    attempt with lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -64,28 +64,41 @@ a = (f"Total Months:{len(month)}")
 b = (f"The total profit/loss:   {total}")
 
 
-# -----------below in comment lines is my approach for change in price and the max/min values------
-# set_1  =[]
+# -----------below in green comment lines is my approach for change in price average and the max/min values------
+#          CALCULATION OF change in price starting at column 2 - column 1
+# set_1  =[]    
 # set_2 = []
-# change = set_2 - set_1
+# change_1st_line = int(pnl[1])-int(pnl[0])
+# change = set_2-set_1 
+
+# #If the row of the pnl is odd add it to set_1
+# # otherwise add it to the even list of set_2
 # for i in pnl:
-  
-#   set_1.append(int(pnl[i]))
-#   set_2.append(int(pnl[i+1]))
+#   count  = count +1 
+#   if (i % 2) == 0:
+#      set_2.append(pnl)
+#   else:
+#      set_1.append(pnl)
+
+
+
+# average = sum(change)/len(change)  #AVERAGE CHANGE IN PRICE
+
+#            PRINT METHOD FOR MAX/MIN such that:
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167) 
+
 # max_change = max(change)
 # min_change =min(change)
-
-# # Greatest Increase in Profits: Feb-2012 ($1926159)
-# # Greatest Decrease in Profits: Sep-2013 ($-2196167)
-# c=  (f"Greatest Increase in Profits:  {max_change}")
-# d = (f"Greatest Decrease in Profits: {min_change}")
-
+# c=  (f"Greatest Increase in Profits: {month[]} ({max_change})")
+# d = (f"Greatest Decrease in Profits: {month[]} ({min_change})")
+#----------------------------------------------------------------------------------------------------------
 print("Financila Analysis")
 print("--------------------------------")
 
 print (a)
 print(b)
-# print(f"Average  Change:   {change}")
+# print(f"Average  Change:   {average}")
 # print(c)
 # print(d)
 
